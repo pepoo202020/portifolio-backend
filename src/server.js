35f4343ffa62e64a8c789a72119d3cfcc85039f3
Abanob Shenoda <121344723+pepoo202020@users.noreply.github.com>
2023-03-05 11:18:40 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -12,11 +12,10 @@ const PORT = process.env.PORT || 5000
 
 connect()
 
+app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors({
-    origin: ['https://mern-portifolio.onrender.com/']
-}))
+
 
 app.get('/', (_req, res) => {
     res.status(200).json({ success: true, message: 'hello from server' })
